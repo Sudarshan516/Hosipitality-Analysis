df7d5dada7c457e75255cd035a42e7c226a731b9
Sudarshan516 <sudarshanneravati@gmail.com>
2025-11-25 20:24:57 +0530

    Add files via upload

---
+++ excelr_project.sql
@@ -0,0 +1,116 @@
+create database project;
+use project;
+show tables;
+
+select  * from fact_aggregated_bookings;
+SET GLOBAL local_infile = 1;
+SHOW GLOBAL VARIABLES LIKE 'local_infile';
+
+
+
+LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/fact_bookings.csv'
+INTO TABLE fact_bookings
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+ALTER TABLE fact_bookings
+CHANGE check_in_date `date` DATE;
+ALTER TABLE fact_bookings 
+CHANGE room_category room_id VARCHAR(20);
+
+
+-- total Revenue
+select concat(round(sum(revenue_realized)/1000000,2),'M') as `Total Revenue` from fact_bookings;
+
+-- total booking
+select concat(round((sum(successful_bookings)/1000),2),'k')`Total Bookings` from fact_aggregated_bookings;
+
+-- Average rating
+select concat(round(avg(ratings_given),2),'%') as `Average Rating`from fact_bookings;
+
+-- Occupancy
+select concat(round((sum(successful_bookings)/sum(capacity))*100,2),'%') as Occupancy from fact_aggregated_bookings;
+
+-- cancellation%
+SELECT 
+   concat( round(COUNT(CASE WHEN booking_status = 'Cancelled' THEN 1 END) * 100
+    / COUNT(*),2),'%') AS `Cancellation`
+FROM fact_bookings;
+
+-- city wise revenue
+select dim_hotels.city,concat(round(sum(fact_bookings.revenue_realized)/1000000,2),'M') as `Total revenue` from dim_hotels
+join
+ fact_bookings on dim_hotels.property_id = fact_bookings.property_id
+ group by dim_hotels.city
+ order by `Total Revenue` desc;
+ select * from dim_date;
+ 
+-- Day Type Revenue
+ SELECT dim_date.day_type,
+      concat(round( SUM(fact_bookings.revenue_realized)/1000000,2),'M') AS `Total Revenue` from dim_date
+left JOIN fact_bookings
+  ON dim_date.date = STR_TO_DATE(TRIM(fact_bookings.date), '%Y-%m-%d')
+GROUP BY dim_date.day_type
+order by `Total Revenue`;
+
+-- class wise revenue
+select dim_rooms.room_class,concat(round(sum(fact_bookings.revenue_realized)/1000000,2),'M') as `Total Revenue`  from dim_rooms
+join 
+	fact_bookings on dim_rooms.room_id = fact_bookings.room_id
+    group by dim_rooms.room_class
+    order by `Total Revenue` desc;
+    
+-- hotel wise revenue
+select dim_hotels.property_name,concat(round(sum(fact_bookings.revenue_realized)/1000000,2),'M') as `Total revenue` from dim_hotels
+join
+ fact_bookings on dim_hotels.property_id = fact_bookings.property_id
+ group by dim_hotels.property_name
+ order by `Total Revenue` desc ;
+ 
+ -- platform wise revenue
+ select booking_platform,concat(round(sum(fact_bookings.revenue_realized)/1000000,2),'M') as `Total revenue` from fact_bookings
+ group by booking_platform;
+
+
+-- booking status analysis
+select  booking_status,concat(round(sum(revenue_realized)/1000000,2),'M') as `Total Revenue`  from fact_bookings
+group by booking_status;
+
+-- weekly trend (Revenue,Cancellation)
+select dt.`week no`,concat(round(sum(fb.revenue_realized)/1000000,2),'M')as `Total Revenue`,
+    concat( round(COUNT(CASE WHEN booking_status = 'Cancelled' THEN 1 END) * 100
+    / COUNT(*),2),'%') AS `Cancellation`
+    from dim_date dt
+left  join 
+ fact_bookings fb on dt.date =  STR_TO_DATE(fb.date, '%Y-%m-%d')
+ group by dt.`week no`
+ order by  dt.`week no`;
+ 
+ 
+ -- weekly trend (bookings,Occupancy)
+select dt.`week no`,
+concat(round((sum(fab.successful_bookings)/1000),2),'k')`Total Bookings`,
+concat(round((sum(successful_bookings)/sum(capacity))*100,2),'%') as Occupancy
+ from dim_date dt
+left join
+fact_aggregated_bookings fab on dt.date = str_to_date(fab.date,'%d-%b-%y')
+group by `week no`;
+ 
+ 
+ -- weekly trend table( losting connection)
+
+select dt.`week no`,concat(round(sum(fb.revenue_realized)/1000000,2),'M')as `Total Revenue`,
+    concat( round(COUNT(CASE WHEN booking_status = 'Cancelled' THEN 1 END) * 100
+    / COUNT(*),2),'%') AS `Cancellation`,
+    concat(round((sum(fab.successful_bookings)/1000),2),'k')`Total Bookings`,
+concat(round((sum(successful_bookings)/sum(capacity))*100,2),'%') as Occupancy
+    from dim_date dt
+left  join 
+ fact_bookings fb on dt.date =  STR_TO_DATE(fb.date, '%Y-%m-%d')
+ left join
+fact_aggregated_bookings fab on dt.date = str_to_date(fab.date,'%d-%b-%y')
+ group by dt.`week no`
+ order by  dt.`week no`;
+ 
